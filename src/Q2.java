cce04c5c30ff7eeaf12faa70a6928fddca2ad87c
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2023-05-03 01:07:14 +0530

    Add some comments as the data given in the quesion

---
+++ src/Q2.java
@@ -0,0 +1,20 @@
+// Closet Pair
+
+//Non-recursive algorithm for the Closest Pair Program
+
+
+//Analyze runtime
+
+
+//Implement algorithm using java
+
+//Test case:
+//        Input: [-5, 9, -3, -2, 0, 1, 10]
+//        Output: (-3, -2), (0, 1) and (9, 10)
+
+public class Q2 {
+
+    public static void main(String[] args) {
+
+    }
+}
