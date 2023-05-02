658cde231355d005ff088068d3403eb6daf337fd
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2023-05-03 01:00:55 +0530

    Add some comments as the data given in the quesion

---
+++ src/Q1.java
@@ -0,0 +1,33 @@
+//local minimum of a matrix
+// a[i][j]
+// 1. write down the algorithm
+
+// 2. Analyze the runtime of the above algorithm
+
+// 3. Implement algorithm using Java
+
+//Test case:
+//        Input:
+//        [[26 27 58 38]
+//        [33 53 70 42]
+//        [5 33 2 66]
+//        [65 55 70 44]]
+//        Output:
+//        26, 38, 44, 5, 2 (Can be printed in any order)
+
+public class Q1 {
+
+    public void checkMin(int a, int b){
+        if (a < b){
+
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0;i < 10; i++){
+            for (int j = 0; j < 10; j++){
+
+            }
+        }
+    }
+}
