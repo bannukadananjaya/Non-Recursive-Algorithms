eb0afb03a0e1641b302746ec18efc12179c6900c
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2023-05-12 06:07:02 +0530

    second commit

--- src/E18/Q1.java
+++ src/E18/Q1.java
@@ -1,33 +1,38 @@
-package E18;//local minimum of a matrix
-// a[i][j]
-// 1. write down the algorithm
+package E18;
 
-// 2. Analyze the runtime of the above algorithm
-
-// 3. Implement algorithm using Java
-
-//Test case:
-//        Input:
-//        [[26 27 58 38]
-//        [33 53 70 42]
-//        [5 33 2 66]
-//        [65 55 70 44]]
-//        Output:
-//        26, 38, 44, 5, 2 (Can be printed in any order)
+import java.util.ArrayList;
+import java.util.List;
 
 public class Q1 {
-
-    public void checkMin(int a, int b){
-        if (a < b){
-
-        }
-    }
-
-    public static void main(String[] args) {
-        for (int i = 0;i < 10; i++){
-            for (int j = 0; j < 10; j++){
-
+    
+    public static List<int[]> findLocalMinimums(int[][] arr) {
+        List<int[]> result = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isLocalMinimum(arr, i, j)) {
+                    result.add(new int[]{i, j});
+                }
             }
+        }
+        return result;
+    }
+    
+    private static boolean isLocalMinimum(int[][] arr, int i, int j) {
+        int n = arr.length;
+        int val = arr[i][j];
+        if (i > 0 && arr[i-1][j] < val) return false; // check up
+        if (j > 0 && arr[i][j-1] < val) return false; // check left
+        if (i < n-1 && arr[i+1][j] < val) return false; // check down
+        if (j < n-1 && arr[i][j+1] < val) return false; // check right
+        return true;
+    }
+    
+    public static void main(String[] args) {
+        int[][] arr = {{8, 7, 6}, {5, 4, 3}, {2, 1, 0}};
+        List<int[]> result = findLocalMinimums(arr);
+        for (int[] pos : result) {
+            System.out.println("(" + pos[0] + ", " + pos[1] + ")");
         }
     }
 }
---
+++ src/E19/Q2.java
@@ -0,0 +1,51 @@
+package E19;
+
+public class Q2 {
+    public static void main(String args[]){
+        
+    }
+}
+
+class array{
+    
+    int length;
+    int n;
+    private int[] array = new int[n];
+
+    //call scanner object
+
+    //call parameterized constructor
+    public array(int n){
+        this.n = n;
+    }
+
+    //getting inputs from the client
+    public void getArray(){
+
+    }
+
+    //print elements of the array
+    public void readArray(){
+
+    }
+
+    //swap elements in array
+    public void swap(){
+
+    }
+
+    //Delete element of a array
+    public void deleteElement(){
+
+    }
+
+    //insert elements into the array
+    public void insertElements(){
+
+    }
+
+    //search elements from the array
+    public void searhElemnts(){
+
+    }
+}
